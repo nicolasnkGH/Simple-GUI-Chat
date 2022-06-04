266b5fafab2f48d1506170aa23a81ec6895094f5
Nicolas <nicolasnk_teixeira@hotmail.com>
2022-06-03 17:08:54 -0700

    Source code for server and client side
    Added the client and server source code to the directory.

---
+++ client.py
@@ -0,0 +1,108 @@
+from base64 import encode
+import socket
+import threading
+import tkinter
+import tkinter.scrolledtext
+from tkinter import simpledialog
+
+# It needs to match the server port
+HOST = '127.0.0.1' 
+PORT = 9090
+
+''' You can also use your public IP address to host on the web instead locally
+    The user will have to specify the public IP address in order to connect
+    Need to open ports on the server side as well
+'''
+
+# Creating a client that has a socket that connects with host and port
+class Client:
+
+    def __init__(self, host, port):
+
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.sock.connect((host, port))
+
+        msg = tkinter.TK()
+        msg.withdraw()
+
+        # Getting nickname from the user
+        self.nickname = simpledialog.askstring("Nickname", "Please choose a nickname", parent=msg)
+
+        self.gui_done = False
+        self.running = True
+
+        # Threads to build the GUI and maintains the GUI and connects to the server
+        gui_thread = threading.Thread(target=self.gui_loop)
+        receive_thread = threading.Thread(target=self.receive)
+
+        gui_thread.start()
+        receive_thread.start()
+
+    # Build the GUI
+    def gui_loop(self):
+        self.win = tkinter.Tk()
+        self.win.configure(bg="lightgray")
+
+        self.chat_label = tkinter.Label(self.win, text="Chat:", bg="lightgray")
+        self.chat_label.config(font=("Arial", 12))
+        self.chat_label.pack(padx=20, pady=5)
+
+        self.text_area = tkinter.scrolledtext.ScrolledText(self.win)
+        self.text_area.pack(padx=20, pady=5)
+
+        # Disabled means the content cannot be changed. To change it, revert it to enabled make 
+        # the changes and revert back to disabled.
+        self.text_area.config(state='disabled') 
+
+        self.msg_label = tkinter.Label(self.win, text="Message:", bg="lightgray")
+        self.msg_label.config(font=("Arial", 12))
+        self.msg_label.pack(padx=20, pady=5)
+
+        self.input_area = tkinter.Text(self.win, height=3)
+        self.input_area.pack(padx=20, pady=5)
+
+        self.send_button = tkinter.Button(self.win, text="Send, command=self.write")
+        self.send_button.config(font=("Arial", 12))
+        self.send_button.pack(padx=20, pady=5)
+
+        self.gui_done = True
+
+        self.win.protocol("WM_DELETE_WINDOW", self.stop)
+
+        self.win.mainloop()
+
+
+    def write(self):
+        message = f"{self.nickname}: {self.input_area.get('1.0', 'end')}"
+        self.sock.send(message.encode('utf-8'))
+        self.input_area.delete('1.0', 'end')
+
+
+    def stop(self):
+        self.running = False
+        self.win.destroy()
+        self.sock.close()
+        exit(0)
+
+
+    def receive(self):
+        while self.running:
+            try:
+                message = self.sock.recv(1024).decode('utf-8')
+                if message == 'NICK':
+                    self.sock.send(self.nickname.encode('utf-8'))
+
+                else:
+                    if self.gui_done:
+                        self.text_area.config(state='normal')
+                        self.text_area.config('end', message)
+                        self.text_area.yview('end')
+                        self.text_area.config(state='disabled')
+            except ConnectionAbortedError:
+                break
+            except:
+                print('Error')
+                self.sock.close()
+                break
+
+client = Client(HOST, PORT)
---
+++ server.py
@@ -0,0 +1,62 @@
+from base64 import encode
+from http import client
+import socket
+import threading
+
+HOST = '127.0.0.1' 
+PORT = 9090
+
+''' You can also use your public IP address to host on the web instead locally
+    The user will have to specify the public IP address in order to connect
+    Need to open ports on the server side as well
+'''
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind((HOST, PORT))
+
+server.listen()
+
+clients = []
+nicknames = []
+
+#broadcast
+def broadcast(message):
+    for client in clients:
+        client.send(message)
+
+#handle
+def handle(client):
+    while True:
+        try:
+            message = client.recv(1024)
+            print(f"{nicknames[clients.index(client)]}")
+
+        except:
+            index = clients.index(client)
+            clients.remove(client)
+            client.close()
+            nickname = nicknames[index]
+            nicknames.remove(nickname)
+            break
+
+#receive (listen for new connections)
+def receive():
+    while True:
+        client, address = server.accept()
+        print(f"Connected with {str(address)}!")
+
+        client.send("NICK".encode('utf-8'))
+        nickname = client.receive(1024)
+
+        nicknames.append(client)
+        clients.append(client)
+
+        print(f"Nickname of the client is {nickname}")
+        broadcast(f"{nickname} connected to the server!\n".encode('utf-8'))
+        client.send("Connected to the server".encode('utf-8'))
+
+        thread = threading.threading.Thread(target=handle, args=(client,))
+        thread.start()
+
+print("Server running...")
+receive()
